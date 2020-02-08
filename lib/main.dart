95a5a6ddf623e9ffff3cd2b5c35851e97fd26d18
rishirajrsawant <rishiraj.sawant97@gmail.com>
2020-02-08 20:16:08 +0000

    v1.1 second commit -added sample image from vecteezy.com -implemented AssetImage() functionality N.B. whenever pubspec.yaml is edited, must remember to do package upgrage

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.amber[200],
       appBar: AppBar(
         title: Text(
           'I Am Poor',
           style:
               TextStyle(color: Colors.deepOrange, fontStyle: FontStyle.italic),
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.amber[500],
       ),
-      body: Center(child: new Image(image: AssetImage(''))),
+      body: Center(
+        child: new Image(
+          image: AssetImage('images/poor.jpg'),
+        ),
+      ),
     ),
   ));
 }
